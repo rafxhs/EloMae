ec76a019e42a107a6b4e81e8aec58b9b5dc58733
hannasabrynna <hannasabrynna135@gmail.com>
2025-09-23 15:27:10 -0300

    feat: criação de estruturas inicias das tabelas: institutions, threads, comments e reminders

---
+++ EloMae/database/migrations/2025_09_23_181443_create_institutions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('type', ['cras','creas', 'prefeitura', 'conseçho', 'ubs','delegacia','ong','creche','outro']);
+            $table->string('address')->nullable();
+            $table->decimal('lat', 10, 7)->nullable();
+            $table->decimal('lng', 10, 7)->nullable();
+            $table->string('phone')->nullable();
+            $table->string('website')->nullable();
+            $table->boolean('verified')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('institutions');
+    }
+};
