d8b7965c0534083ffa1f3c4d283b75d0e7d64f92
Erick Souza <erickdasilvasouza824@gmail.com>
2026-01-27 16:43:09 -0300

    fix: corrigindo erros do console no navegador

--- EloMae/resources/js/Pages/Articles/Index.jsx
+++ EloMae/resources/js/Pages/Articles/Index.jsx
@@ -22,25 +22,24 @@ export default function Index() {
     return (
         <AuthenticatedLayout user={auth.user}>
             <div className="max-w-4xl mx-auto py-10 relative">
-
                 {user && user.is_admin ? (
                     <LinkButton
-                        href={route('articles.create')}
+                        href={route("articles.create")}
                         className="absolute left-4 flex items-center justify-center"
                     >
                         <HiPlus className="h-6 w-6" />
                     </LinkButton>
-                ):null}
+                ) : null}
 
                 <div className="flex items-center justify-between gap-10 mb-6 mt-6 border-b pb-4 shadow-lg">
-
                     <div className="max-w-md">
-                            <h1 className="text-3xl font-bold text-purple-600">
-                                Artigos
-                            </h1>
-                            <p className="mt-2 text-gray-600">
-                                Explore nossos artigos sobre maternidade, direitos, bem-estar, desenvolvimento infantil e muito mais.
-                            </p>
+                        <h1 className="text-3xl font-bold text-purple-600">
+                            Artigos
+                        </h1>
+                        <p className="mt-2 text-gray-600">
+                            Explore nossos artigos sobre maternidade, direitos,
+                            bem-estar, desenvolvimento infantil e muito mais.
+                        </p>
                     </div>
 
                     <img
@@ -51,8 +50,7 @@ export default function Index() {
                 </div>
 
                 <form onSubmit={handleSearch} className="mb-6 flex gap-2">
-
-                    <div className='relative bg-gray-200 rounded-lg mt-4 flex-[1]'>
+                    <div className="relative bg-gray-200 rounded-lg mt-4 flex-[1]">
                         <input
                             type="text"
                             placeholder="Pesquisar artigos..."
@@ -65,7 +63,7 @@ export default function Index() {
                         </button>
                     </div>
 
-                    <div className='relative bg-gray-200 rounded-lg mt-4 ' >
+                    <div className="relative bg-gray-200 rounded-lg mt-4 ">
                         <select
                             value={category}
                             onChange={(e) => {
@@ -92,25 +90,28 @@ export default function Index() {
                 <div className="space-y-4">
                     {articles.length === 0 ? (
                         <div className="text-center text-gray-500 py-10 border hover:text-gray-500 rounded bg-white shadow flex flex-col items-center gap-4">
-
                             <HiSearch className="h-12 w-12 text-gray-400" />
 
-                            <p>Ops!... Nenhum artigo encontrado para sua pesquisa.</p>
-                            <p>Tente pesquisar por categoria, tags ou use o filtro de pesquisa.</p>
+                            <p>
+                                Ops!... Nenhum artigo encontrado para sua
+                                pesquisa.
+                            </p>
+                            <p>
+                                Tente pesquisar por categoria, tags ou use o
+                                filtro de pesquisa.
+                            </p>
 
-                            <LinkButton
-                                href={route("articles.index")}
-                            >
+                            <LinkButton href={route("articles.index")}>
                                 Limpar e voltar
                             </LinkButton>
                         </div>
                     ) : (
                         articles.map((article) => (
-                            <div className="p-4 bg-white border hover:shadow-purple-200 shadow rounded hover:shadow-lg transition cursor-pointer block">
-                                <Link
-                                    key={article.id}
-                                    href={route("articles.show", article.id)}
-                                >
+                            <div
+                                key={article.id}
+                                className="p-4 bg-white border hover:shadow-purple-200 shadow rounded hover:shadow-lg transition cursor-pointer block"
+                            >
+                                <Link href={route("articles.show", article.id)}>
                                     <p className="text-xs text-gray-500">
                                         Autor: {article.author.name}
                                     </p>
@@ -120,9 +121,7 @@ export default function Index() {
                                     </h2>
 
                                     {article.subtitle && (
-                                        <p className="">
-                                            {article.subtitle}
-                                        </p>
+                                        <p>{article.subtitle}</p>
                                     )}
 
                                     <p className="text-xs text-gray-400 mt-0">
@@ -130,7 +129,6 @@ export default function Index() {
                                     </p>
 
                                     <p className="mt-3">{article.summary}</p>
-
                                 </Link>
                             </div>
                         ))
