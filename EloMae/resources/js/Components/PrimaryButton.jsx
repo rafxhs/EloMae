0e48eca3c3aad5f097d19e19b3879705a500a5f3
Rafaela Neves <rafaelaneves2712@hotmail.com>
2026-02-04 05:58:55 -0300

    feature(frontend): estilizacao da pagina de login e register + onboarding

--- EloMae/resources/js/Components/PrimaryButton.jsx
+++ EloMae/resources/js/Components/PrimaryButton.jsx
@@ -8,7 +8,7 @@ export default function PrimaryButton({
         <button
             {...props}
             className={
-                `inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 ${
+                `w-full sm:h-[45px] items-center rounded-lg border border-transparent bg-primary-400 px-4 py-2 text-sm font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-primary-300 focus:outline-none active:bg-primary-500 ${
                     disabled && 'opacity-25'
                 } ` + className
             }
--- EloMae/resources/js/Layouts/GuestLayout.jsx
+++ EloMae/resources/js/Layouts/GuestLayout.jsx
@@ -1,16 +1,9 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 px-4 sm:justify-center sm:pt-0">
+            <div className="mt-6 w-full min-h-[600px] overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-4xl  sm:rounded-2xl">
                 {children}
             </div>
         </div>
